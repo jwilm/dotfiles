7138b2926370f54566db4b5c719a09e00646113e
Joe Wilm <joe@gameclosure.com>
2014-06-12 23:16:28 -0700

    Add tmux config

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/vim/ ~/.vim
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 
