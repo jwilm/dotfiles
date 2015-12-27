35b8d3e4f399a149533def545d4c975b8a1c08a0
Joe Wilm <joe@jwilm.com>
2015-12-26 22:28:40 -0800

    Add build step for YouCompleteMe

--- install.sh
+++ install.sh
@@ -19,3 +19,16 @@ symlink_dotfile "zshrc"
 
 tmux/install.sh
 system_setup/configure_git.sh
+
+function build_ycm_completers() {
+    local force=$1
+    local ycm_path=$script_dir/vim/bundle/YouCompleteMe
+    local ycmd_path=$ycm_path/third_party/ycmd
+    local gocode=$ycmd_path/third_party/gocode/gocode
+
+    if [[ $force || ! -z $gocode ]] ; then
+        $ycm_path/install.py --gocode-completer --clang-completer
+    fi
+}
+
+build_ycm_completers
